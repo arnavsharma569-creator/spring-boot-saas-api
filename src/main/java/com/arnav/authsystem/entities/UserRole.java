9be0f0cc7b64afa66ba11b902bc26a5f8d47f7af
Arnav Sharma <s4099050@student.rmit.edu.au>
2026-06-25 03:52:26 +1000

    feat: UserRole entity with many-to-many mapping

---
+++ src/main/java/com/arnav/authsystem/entities/UserRole.java
@@ -0,0 +1,22 @@
+package com.arnav.authsystem.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
+
+}
