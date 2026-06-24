ed1886f63a3b28c45d6bc7e14a1dfe58e871f461
Arnav Sharma <s4099050@student.rmit.edu.au>
2026-06-25 03:53:45 +1000

    feat: request DTOs for auth and refresh token

---
+++ src/main/java/com/arnav/authsystem/dto/UserInfoDto.java
@@ -0,0 +1,28 @@
+package com.arnav.authsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserInfoDto {
+
+    private String userId;
+
+    private String username;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Long phoneNumber;
+
+    private String email;
+
+}
